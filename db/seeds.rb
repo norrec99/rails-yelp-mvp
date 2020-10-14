d568efd22e561f91c8560d114615d49ac2b92c22
Cagatay Soyer <cagataysoyer@gmail.com>
2020-10-14 14:34:18 +0300

    restaurant and review model created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+puts "Cleaning the database..."
+Restaurant.destroy_all
+
+
+puts "Creating 100 restaurants..."
+100.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: rand(1..5)
+  )
+  restaurant.save!
+  puts "#{restaurant.name} created!"
+end
+
+puts "Seeded ended!"
